a28fea2d144d4b66e2292098bf791dc21a09fa33
nahokoxxx <me@nahoko.dev>
2023-02-01 13:31:08 +0900

    Vuetifyの全コンポーネントを登録

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,10 +1,12 @@
 import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
 
+import * as components from "vuetify/components";
 import { createVuetify } from "vuetify";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  components,
   theme: {
     themes: {
       light: {
